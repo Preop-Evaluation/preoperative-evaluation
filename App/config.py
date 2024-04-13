384f8be10cef90a4d3bd6e42955b7801fceb443f
devonxmurray <166621170+Talli-24@users.noreply.github.com>
2024-04-13 17:50:23 -0400

    Update config.py

--- App/config.py
+++ App/config.py
@@ -3,6 +3,9 @@ import os
 def load_config(app, overrides):
     if os.path.exists(os.path.join('./App', 'custom_config.py')):
         app.config.from_object('App.custom_config')
+    elif os.environ.get('ENV') == 'PRODUCTION':
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+        app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     else:
         app.config.from_object('App.default_config')
     app.config.from_prefixed_env()
