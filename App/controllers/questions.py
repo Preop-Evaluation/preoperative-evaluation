275347cf7038f3ad0cb17de12d02b341cdad14e4
JustaSingular <mitchellpeterdaniel@gmail.com>
2024-04-15 02:56:58 -0400

    fixes and quality of life improvements
    fixes to broken questionnaire and changes to patient phone number code from 10 numbers to 7

--- App/controllers/questions.py
+++ App/controllers/questions.py
@@ -1,13 +1,13 @@
 def get_default_questionnaire():
     default_questions = [
         {
-            "id": "q0",
+            "id": "q1",
             "text": "Please describe your current health condition.",
             "type": "long_answer",
-            "follow_ups": []
+            "conditional_on": True
         },
         {
-            "id": "q1",
+            "id": "q2",
             "text": "Do you have Diabetes (Sugar)?",
             "type": "boolean",
             "follow_ups": [
@@ -15,59 +15,43 @@ def get_default_questionnaire():
                     "id": "q2a",
                     "text": "For how long?",
                     "type": "long_answer",
-                    "conditional_on": "Other"
+                    "conditional_on": True
                 }
             ]
         },
         {
-            "id": "q2",
-            "text": "Is your condition managed (treated) [controlled] by Diet / Tablets / Insulin",
-            "type": "multiple_choice",
-            "choices": ["Diet", "Tablets", "Insulin"],
-        },
-        {
             "id": "q3",
-            "text": "What medication are you on?",
-            "type": "long_answer",
-        },
-        {
-            "id": "q4",
             "text": "Do you have hypertension (high blood pressure)?",
             "type": "boolean",
             "follow_ups": [
                 {
-                    "id": "q4a",
-                    "text": "Is your condition managed (treated) [controlled] by Diet / Tablets ",
-                    "type": "long_answer",
-                    "conditional_on": True,
+                "id": "q3a",
+                "text": "Is your condition managed (treated) [controlled] by Diet / Tablets ",
+                "type": "long_answer",
+                "conditional_on": True
                 }
             ]
         },
-          {
-            "id": "q5",
-            "text": "What medication are you on for the hypertension?",
-            "type": "long_answer",
-            "conditional_on": True
-        },
         {
-            "id": "q6",
+            "id": "q4",
             "text": "Do you have high cholesterol?",
             "type": "boolean",
             "follow_ups": [
                 {
-                    "id": "q6a",
+                    "id": "q4a",
                     "text": "Are you on medication?",
                     "type": "boolean",
+                    "conditional_on": True
                 }
             ]
         },
         {
-            "id": "q7",
+            "id": "q5",
             "text": "Have you ever had a myocardial infarction (heart attack)?",
             "type": "boolean",
             "follow_ups": [
                 {
-                    "id": "q7a",
+                    "id": "q5a",
                     "text": "How long ago?",
                     "type": "long_answer",
                     "conditional_on": True
@@ -75,12 +59,12 @@ def get_default_questionnaire():
             ]
         },
         {
-            "id": "q8",
+            "id": "q6",
             "text": "Do you have ischaemic heart disease (angina)?",
             "type": "boolean",
             "follow_ups": [
                 {
-                    "id": "q8a",
+                    "id": "q6a",
                     "text": "For how long?",
                     "type": "long_answer",
                     "conditional_on": True
@@ -89,12 +73,12 @@ def get_default_questionnaire():
     
         },
          {
-            "id": "q9",
+            "id": "q7",
             "text": "Did you ever have a stroke or TIA?",
             "type": "boolean",
             "follow_ups": [
                 {
-                "id": "q9a",
+                "id": "q7a",
                 "text": "How long?",
                 "type": "long_answer",
                 "conditional_on": True
@@ -103,12 +87,12 @@ def get_default_questionnaire():
             ]
         },
          {
-            "id": "q10",
+            "id": "q8",
             "text": "Do you have heart failure? ",
             "type": "boolean",
             "follow_ups": [
                 {
-                    "id": "q10a",
+                    "id": "q8a",
                     "text": "How long? ",
                     "type": "long_answer",
                     "conditional_on": True
