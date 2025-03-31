0a6e48f47097d3afaea51e4ea8bc064f9676da7a
Rajiv-INFO3604 <130197749+Rajiv-INFO3604@users.noreply.github.com>
2025-03-31 03:06:09 -0400

    Update token.py

--- App/token.py
+++ App/token.py
@@ -5,7 +5,7 @@ def generate_reset_token(email):
     serializer = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
     return serializer.dumps(email, salt='password-reset-salt')
 
-def verify_reset_token(token, expiration=3600):
+def verify_reset_token(token, expiration=3600): # 3600s = 1 hour
     serializer = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
     try:
         email = serializer.loads(token, salt='password-reset-salt', max_age=expiration)
