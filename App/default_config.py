6bdfffbfb887abb3b1d75465a310c5b56788d9c1
Rajiv-INFO3604 <130197749+Rajiv-INFO3604@users.noreply.github.com>
2025-04-13 03:27:25 -0400

    Add files via upload

---
+++ App/default_config.py
@@ -0,0 +1,10 @@
+SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+SECRET_KEY = "secret key"
+
+# --- Mail Configuration ---
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USERNAME = 'rajiv.lakhan25@gmail.com'
+MAIL_PASSWORD = 'ufos jeke orci ymjy'
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
