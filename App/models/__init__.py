0d04fe7da1ad32b4176e122c8f68162b3091830d
Rajiv-INFO3604 <130197749+Rajiv-INFO3604@users.noreply.github.com>
2025-04-13 03:29:08 -0400

    Delete App/models directory

--- App/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .user import *
-from .patient import *
-from .doctor import *
-from .anesthesiologist import *
-from .questionnaire import *
-from .notification import *
-from .admin import Admin
-
